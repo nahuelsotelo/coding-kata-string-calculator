b8d75ef90de51a03c846eabefc351d36e3373bca
Nahuel Sotelo <nahuelsotelo@gmail.com>
2015-06-24 20:57:52 +0200

    remove comments

--- test/test.js
+++ test/test.js
@@ -1,12 +1,8 @@
 var assert = require("assert"),
 stringCalculator = require('../js/string-calculator').stringCalculator;
 
-describe('stringCalculator', function(){
+describe('stringCalculator', function (done){
     it('should return 0 when recive an empty string', function(){
         assert.equal(0, stringCalculator(""));
     })
-
-    // it('', function(){
-    //     assert.equal(0, stringCalculator(""));
-    // })
 })
